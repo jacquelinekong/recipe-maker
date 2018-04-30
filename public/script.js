1c62b77e47b6f6a321016f3484acb6370f52e766
Jacqueline Kong <jek2179@columbia.edu>
2018-04-29 22:45:25 -0400

    Added sortable

--- public/script.js
+++ public/script.js
@@ -6,24 +6,26 @@ function takeScreenshot(source, dest) {
   var canvas = document.createElement("canvas");
   var ctx = canvas.getContext('2d');
   var new_image = document.createElement("img");
-  var new_div = $("<div>")
+  var new_item = $("<li class=\"screenshot\">");
 
   ctx.drawImage(source, 0, 0, canvas.width, canvas.height);
   // dest.append(canvas);
   var data = canvas.toDataURL('image/jpeg');
   new_image.src = data;
 
-  new_div.append(new_image);
-  dest.append(new_div);
+  new_item.append(new_image);
+  dest.append(new_item);
   dest.scrollTop(dest.prop('scrollHeight'));
 }
 
 function addNote(dest) {
   var note = $("#note-form").val();
   var new_note = $("<div>");
+  var new_item = $("<li>");
   $(new_note).text(note);
 
-  dest.append(new_note);
+  $(new_item).append(new_note);
+  dest.append(new_item);
   $("#note-form").val("");
   dest.scrollTop(dest.prop('scrollHeight'));
 }
@@ -38,26 +40,7 @@ $(document).ready(function() {
   video.pause();
   $("#pause").hide();
 
-  // /* play/pause with spacebar */
-  // $(window).on("keypress", (function(e) {
-  //   /* https://stackoverflow.com/questions/26981018/how-to-play-pause-html5-video-with-spacebar */
-  //
-  //   if (e.which == 32) {
-  //     if (video.paused == true)
-  //       video.play();
-  //     else
-  //       video.pause();
-  //   }
-  // }))
-
-  /* submit note with enter -- should implement? */
-  // $("#note-form").focus(function() {
-  //   $(window).on("keypress", (function(e) {
-  //     if (e.which == 13) {
-  //       console.log("enter");
-  //     }
-  //   }))
-  // })
+  $("#recipe-list").sortable();
 
   $("#capture").on("click", function() {
     video.pause();
